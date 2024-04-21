14d75341346e54c798d97456c357058da8bb40e9
Priyanshu Das <priyanshudas01@gmail.com>
2024-04-21 11:47:39 +0530

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,11 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
 
 // modal variable
 const modalImg = document.querySelector("[data-modal-img]");
